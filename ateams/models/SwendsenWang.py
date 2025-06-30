b8cf4ad26253a6db3f805bb66e2a90f9bdf1fac7
apizzimenti <apizzime@gmu.edu>
2025-06-30 11:21:49 -0400

    LinBox hooks

--- ateams/models/SwendsenWang.py
+++ ateams/models/SwendsenWang.py
@@ -1,7 +1,7 @@
  
 import numpy as np
 
-from ..arithmetic import evaluateCochain, FINT, KernelSample, MatrixReduction
+from ..arithmetic import evaluateCochain, FINT, KernelSample, MatrixReduction, Fast
 from ..stats import constant
 from .Model import Model
 
@@ -9,7 +9,10 @@ from .Model import Model
 class SwendsenWang(Model):
 	name = "SwendsenWang"
 	
-	def __init__(self, L, temperatureFunction=constant(-0.6), initial=None, sparse=True, parallel=False, minBlockSize=32, maxBlockSize=64, cores=4):
+	def __init__(
+			self, L, temperatureFunction=constant(-0.6), initial=None, sparse=True,
+			parallel=False, minBlockSize=32, maxBlockSize=64, cores=4, LinBox=False
+		):
 		"""
 		Initializes Swendsen-Wang evolution on the Potts model.
 
@@ -19,13 +22,14 @@ class SwendsenWang(Model):
 				takes a single positive integer argument `t`, and returns the
 				scheduled temperature at time `t`.
 			initial (galois.FieldArray): A vector of spin assignments to components.
-			sparse (boolean): Should matrices be formatted sparsely? (Uses C/C++).
-			parallel (boolean): Should matrix computations be done in parallel? (Uses C/C++).
+			sparse (bool=True): Should matrices be formatted sparsely? (Uses C/C++).
+			parallel (bool=False): Should matrix computations be done in parallel? (Uses C/C++).
 			minBlockSize (int=32): If `parallel` is truthy, this is the smallest
 				number of columns processed in parallel.
 			maxBlockSize (int=64): If `parallel` is truthy, this is the largest
 				number of columns processed in parallel.
 			cores (int=4): Number of available CPUs/cores/threads on the machine.
+			LinBox (bool=False): Uses fast LinBox routines.
 		"""
 		self.lattice = L
 		self.temperatureFunction = temperatureFunction
@@ -42,6 +46,7 @@ class SwendsenWang(Model):
 		self.Reducer = MatrixReduction(self.lattice.field.characteristic, parallel, minBlockSize, maxBlockSize, cores)
 		self.identity = np.identity(self.coboundary.shape[1], dtype=FINT)
 		self.SampleFromKernel = KernelSample
+		self.LinBox = LinBox
 
 		# SW defaults.
 		self.spins = initial if initial else self.initial()
@@ -82,17 +87,16 @@ class SwendsenWang(Model):
 		satisfied = np.zeros(len(self.lattice.boundary[self.lattice.dimension])).astype(int)
 		satisfied[zeros] = 1
 
-		# subbasis = np.concatenate([
-		# 	self.coboundary.take(zeros, axis=0).T,
-		# 	self.identity
-		# ], axis=1, dtype=FINT)
-		# self.Reducer.RREF(subbasis, len(zeros))
-		spins = self.SampleFromKernel(self.Reducer, self.coboundary.take(zeros, axis=0))
-		spins = self.lattice.field(spins)
-
 		# Uniformly randomly sample a cocycle on the sublattice admitted by the
 		# chosen edges; reconstruct the labeling on the entire lattice by
 		# subbing in the values of c which differ from existing ones.
+		if not self.LinBox:
+			spins = self.SampleFromKernel(self.Reducer, self.coboundary.take(zeros, axis=0))
+		else:
+			spins = Fast(self.coboundary.take(zeros, axis=0), self.lattice.field.characteristic)
+
+		spins = self.lattice.field(spins)
+
 		return spins, satisfied
 	
 
@@ -107,82 +111,3 @@ class SwendsenWang(Model):
 			None.
 		"""
 		self.spins = cocycle
-
-
-# class _SwendsenWang(Model):
-# 	name = "SwendsenWang"
-	
-# 	def __init__(self, L, temperatureFunction=constant(-0.6), initial=None):
-# 		"""
-# 		Initializes Swendsen-Wang evolution on the Potts model.
-
-# 		.. deprecated:: 1.0.2
-
-# 		Args:
-# 			L: The `Lattice` object on which we'll be running experiments.
-# 			temperatureFunction (Callable): A temperature schedule function which
-# 				takes a single positive integer argument `t`, and returns the
-# 				scheduled temperature at time `t`.
-# 			initial (galois.FieldArray): A vector of spin assignments to components.
-# 		"""
-# 		self.lattice = L
-# 		self.temperatureFunction = temperatureFunction
-# 		self.faceCells = len(self.lattice.boundary[self.lattice.dimension-1])
-# 		self.cubeCells = len(self.lattice.boundary[self.lattice.dimension])
-
-# 		# SW defaults.
-# 		self.spins = initial if initial else self.initial()
-
-
-# 	def initial(self):
-# 		"""
-# 		Computes an initial state for the model's Lattice.
-
-# 		Returns:
-# 			A Galois `Array` representing a vector of spin assignments.
-# 		"""
-# 		return self.lattice.field.Random(self.faceCells)
-	
-
-# 	def proposal(self, time):
-# 		"""
-# 		Proposal scheme for generalized Swendsen-Wang evolution on the Potts model.
-
-# 		Args:
-# 			time (int): Step in the chain.
-
-# 		Returns:
-# 			A Galois `Array` representing a vector of spin assignments.
-# 		"""
-# 		# Compute the probability of choosing any individual cube in the complex.
-# 		self.temperature = self.temperatureFunction(time)
-# 		p = 1-np.exp(self.temperature)
-# 		assert 0 <= p <= 1
-
-# 		# Choose cubes to include; in effect, this just does a boatload of indexing.
-# 		uniforms = np.random.uniform(size=self.cubeCells)
-# 		include = (uniforms < p).nonzero()[0]
-# 		boundary = self.lattice.boundary[self.lattice.dimension][include]
-# 		boundaryValues = evaluateCochain(boundary, self.spins)
-# 		zeros = (boundaryValues == 0).nonzero()[0]
-
-# 		satisfied = np.zeros(len(self.lattice.boundary[self.lattice.dimension])).astype(int)
-# 		satisfied[zeros] = 1
-
-# 		# Uniformly randomly sample a cocycle on the sublattice admitted by the
-# 		# chosen edges; reconstruct the labeling on the entire lattice by
-# 		# subbing in the values of c which differ from existing ones.
-# 		return sampleFromKernel(self.lattice.matrices.coboundary, self.lattice.field, includes=zeros), satisfied
-	
-
-# 	def assign(self, cocycle):
-# 		"""
-# 		Updates mappings from faces to spins and cubes to occupations.
-
-# 		Args:
-# 			cocycle (galois.FieldArray): Cocycle on the sublattice.
-		
-# 		Returns:
-# 			None.
-# 		"""
-# 		self.spins = cocycle
